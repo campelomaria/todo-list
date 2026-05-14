68e7a9d98290c79e1be9ec01055093a1484486a8
campelomaria <maria.souza45@estudante.ifms.edu.br>
2026-05-14 10:55:26 -0400

    genki

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,1 +1,28 @@
-//aqui é a estratégia né amore
+import 'dotenv/config';
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+//aqui é a lógica de como o jwt vai ser validado, ou seja, como o token vai ser verificado 
+type JwtPayload = {
+  sub: string; //sub é o id do usuário
+  email: string;
+};
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET as string,
+    });
+  }
+
+  validate(payload: JwtPayload) {
+    return {
+        id: payload.sub,
+        email: payload.email,
+    };
+  }
+}
